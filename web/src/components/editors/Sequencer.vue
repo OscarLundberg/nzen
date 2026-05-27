d4a65310c17ba62fb805e6bfe3c02b6c3efa20f9
Oscar Lundberg <info@oscarlundberg.com>
2026-05-27 11:35:10 +0200

    feat: block out audio tool

---
+++ web/src/components/editors/Sequencer.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+
+</script>
+
+
+<template>
+	<div class="fm-synth">        <-- simple FM synth, a few patches -->
+		<input type="number"/> 
+	</div>
+	<div class="wavetable-synth"> <--- semi-modular wavetable synth, 2 osc linked - detunable, 1 filter hardwired, 1 filter lfo, 1 filter envelope -->
+	<div class="groovebox"></div> <-- 4 basic drum patches -->
+	<div class="fx"></div> <!-- plug to in or out -->
+	<div class="looper"></div> <-- single track ? antything can be plugged in basic ditto style -->
+	<div class="tapedeck"></div> <-- ~4 independent tracks, anything can plug into any track. track mixdown possible (many-to-one) -->
+	<div class="quantized-delay"></div> <!-- set bpm, select whole/half/4th/8ths/16ths  affects all controls globally -->
+	<div class="sequencer"></div> <!-- step sequencer // tracker style sequencer. plugs into either synth -->
+</template>
